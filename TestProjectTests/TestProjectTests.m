5b2b33355dbe554fc7fc45a182d9f53aab2546f6
Bas <bas@oozou.com>
2015-07-11 21:32:14 +0700

    add unit test

--- TestProjectTests/TestProjectTests.m
+++ TestProjectTests/TestProjectTests.m
@@ -22,4 +22,71 @@
     [super setUp];
 }
 
+- (void)testBookShelfShouldHaveDefaultTwoBooks {
+    Book *hungerGames = [[Book alloc] initWithTitle:@"Hunger Games"
+                                            andISBN:@"ashdayd889832dudhwheu"];
+    Book *iosProgramming = [[Book alloc] initWithTitle:@"iOS Programming"
+                                               andISBN:@"dsjfs;gkf324rrgioj"];
+    
+    BookShelf *shelf = [[BookShelf alloc] initWithBooks:@[hungerGames, iosProgramming]];
+    XCTAssertEqual(shelf.books.count, 2);
+}
+
+- (void)testAddBookToShelf {
+    Book *harryPotter = [Book bookWithTitle:@"Harry Potter" andISBN:@"joi2j3oi4j32o4j234"];
+    
+    BookShelf *shelf = [[BookShelf alloc] initWithBooks:@[]];
+    NSInteger amountBeforeAdd = shelf.books.count;
+    
+    [shelf addBook:harryPotter];
+    
+    BOOL found = NO;
+    for (Book *book in shelf.books) {
+        if ([book.title isEqualToString:harryPotter.title]) {
+            found = YES;
+            break;
+        }
+    }
+    
+    XCTAssertEqual(shelf.books.count, amountBeforeAdd + 1);
+    XCTAssertTrue(found == YES);
+}
+
+- (void)testShouldFoundHungerGamesBookInShelf {
+    Book *hungerGames = [[Book alloc] initWithTitle:@"Hunger Games"
+                                            andISBN:@"ashdayd889832dudhwheu"];
+    Book *iosProgramming = [[Book alloc] initWithTitle:@"iOS Programming"
+                                               andISBN:@"dsjfs;gkf324rrgioj"];
+    
+    BookShelf *shelf = [[BookShelf alloc] initWithBooks:@[hungerGames, iosProgramming]];
+    NSInteger amountBeforeTaken = shelf.books.count;
+    Book *foundBook = [shelf bookWithTitle:@"Hunger Games"];
+    
+    XCTAssertEqual(shelf.books.count, amountBeforeTaken - 1);
+    XCTAssertNotNil(foundBook);
+    XCTAssertTrue([foundBook.title isEqualToString:@"Hunger Games"]);
+}
+
+- (void)testShouldNotFoundHarryPotterBookInShelf {
+    Book *hungerGames = [[Book alloc] initWithTitle:@"Hunger Games"
+                                            andISBN:@"ashdayd889832dudhwheu"];
+    BookShelf *shelf = [[BookShelf alloc] initWithBooks:@[hungerGames]];
+    
+    Book *notFoundBook = [shelf bookWithTitle:@"Harry Potter"];
+    XCTAssertNil(notFoundBook);
+}
+
+- (void)testShouldSortByTitleInAscendingOrderAfterAddedNewBook {
+    Book *hungerGames = [[Book alloc] initWithTitle:@"Hunger Games"
+                                            andISBN:@"ashdayd889832dudhwheu"];
+    Book *iosProgramming = [[Book alloc] initWithTitle:@"iOS Programming"
+                                               andISBN:@"dsjfs;gkf324rrgioj"];
+    
+    BookShelf *shelf = [[BookShelf alloc] initWithBooks:@[iosProgramming]];
+    [shelf addBook:hungerGames];
+    
+    XCTAssertTrue([((Book *)shelf.books.firstObject).title isEqualToString:hungerGames.title]);
+    XCTAssertTrue([((Book *)shelf.books.lastObject).title isEqualToString:iosProgramming.title]);
+}
+
 @end
